f2f0ed1b89434d3a45b97207475d6e29455af016
hankhank10 <m.a.hankinson@gmail.com>
2020-06-21 18:00:24 +0100

    1

--- example-code.py
+++ example-code.py
@@ -5,10 +5,10 @@ import scrap  # this needs to be done on every script you write to a scrap from
 ### Link this folder to a scrap key
 # This command only needs to be invoked once for each project directory you work in - it will save the key to a file for use going forward
 # but there's no harm in running it each time if you prefer - it will overwrite nicely
-scrap.setup("scrap-testing")
+scrap.setup("black-ferret")
 
 # Then you write to your scrap with the following command:
-scrap.write("Hello world")
+scrap.write("Hello world: colon and")
 
 # It will appear immediately at your scrap on the web
 
--- scrap.py
+++ scrap.py
@@ -3,15 +3,16 @@
 import os.path
 import sys
 import urllib.request
+import urllib.parse
 
 api_url = "http://scrap.hankapi.com"
 scrap_key_filename = "scrap_key"
 
 def get_key_from_file():
     if os.path.isfile(scrap_key_filename):
-        f = open (scrap_key_filename, "r")
-        key = f.read()
-        f.close
+        with open ("scrap_key", "r") as f:
+            key = f.read()
+
         return key
     else:  
         print ("Error: scrap_key setup file does not exist. Invoke scrap.setup (key-name) from a python script to set.")
@@ -22,18 +23,22 @@ def write(text):
     if key == "ERROR":
         return
     else:
+        url = api_url + "/write?key=" + key + "&text=" + text
+        url = urllib.parse.quote (url,safe=":/&?=")
+        print (url)
         try:
-            url = api_url + "/write/?key=" + key + "&text=" + text
-            with urllib.request.urlopen(api_url + "/write/?key=" + key + "&text=" + text) as response:
-                html = response.read()
-
-        except:
-            print ("scrap error")
+            with urllib.request.urlopen(url) as output:
+                print (output.read), 
+        except Exception as e:
+            print (e.message, e.args)
 
 def setup(key):
-    f = open (scrap_key_filename, "w")
-    f.write (key)
-    f.close
+    with open ("scrap_key", "w") as f:
+        f.write (key)
+
+    #f = open (scrap_key_filename, "w")
+    #f.write (key)
+    #f.close
     write ("New python client writing to scrap " + key)
 
 
